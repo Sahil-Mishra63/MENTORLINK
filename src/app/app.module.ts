edf82dd191b498df3b99d546b2fbd318a6ec1626
Sahil-Mishra63 <sahilmishrayt63@gmail.com>
2024-09-03 10:39:39 +0530

    Added the find-mentor page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -26,7 +27,8 @@ import { FindMentorComponent } from './find-mentor/find-mentor.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [
     provideClientHydration()
--- src/app/chat-page/chat-page.component.ts
+++ src/app/chat-page/chat-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ChatService } from '../chat.service';
 import { Observable } from 'rxjs';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-chat-page',
@@ -10,18 +11,26 @@ import { Observable } from 'rxjs';
 export class ChatPageComponent implements OnInit{
 
     messageInput: string = '';
-    message$: Observable<{ name: string; message: string}>;
+    message$:Observable<{ name: string; message: string}>;
+    // message$: Observable<message[]>;
 
     constructor(private chatService: ChatService) {
       this.message$ = this.chatService.getMessage();
     }
 
     ngOnInit(): void {
-        const name = prompt("Enter your name to join");
+        // if(typeof window !== 'undefined') {
+        //   const name = window.prompt("Enter your name to join");
 
-       if(name) {
-        this.chatService.joinChat(name);
-       }
+          const name = 'Anonymous';
+          if(name) {
+            this.chatService.joinChat(name);
+           }
+        // }
+        // else{
+        //   console.log("Prompt is not available in this environment");
+        // }
+
     }
 
     sendMessage(): void {
